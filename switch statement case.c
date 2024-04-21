ce823874718b661379755513b662a6bd547275e2
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-04-21 21:16:08 +0530

    Add files via upload

---
+++ switch statement case.c
@@ -0,0 +1,20 @@
+//switch case statement
+
+#include<stdio.h>
+int main()
+{
+    int a=5;
+switch(a)
+
+{
+  case 2:
+  printf("value is 5");
+
+  case 3:
+  printf("value is 6");
+
+  default:
+  printf("noting same");
+}
+ return 0;
+}
